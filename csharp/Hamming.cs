d03f75efd41a16a7fcbbf12b641f570940904720
rajaboja <rajeshbojja@gmail.com>
2025-04-21 19:10:44 +0800

    Add Hamming distance calculation implementation in C#

---
+++ csharp/Hamming.cs
@@ -0,0 +1,10 @@
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        if (firstStrand.Length!=secondStrand.Length){
+            throw new ArgumentException();
+        }
+        return firstStrand.Zip(secondStrand, (c1, c2) => c1 != c2).Count(isNotEqual => isNotEqual);
+    }
+}
